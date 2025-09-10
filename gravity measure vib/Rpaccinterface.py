d17f32b55b05324c201ef8cd29263ff78eda6f54
KirillAdILF <k.adamov@g.nsu.ru>
2025-09-10 19:16:11 +0700

    10.09.25

---
+++ gravity measure vib/Rpaccinterface.py
@@ -0,0 +1,17 @@
+import sys
+import redpitaya_scpi as scpi
+import matplotlib.pyplot as plt
+import time
+import os
+from PyQt5.QtWidgets import (QWidget, QToolTip,
+    QPushButton, QApplication)
+from PyQt5.QtGui import QFont
+
+def Main()
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = Main()
+    sys.exit(app.exec_())
