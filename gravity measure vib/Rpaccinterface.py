c2189be9f1852940e92096a9d31ff693ede110cd
KirillAdILF <k.adamov@g.nsu.ru>
2025-09-11 19:00:49 +0700

    11 09 25 lab evening

--- gravity measure vib/Rpaccinterface.py
+++ gravity measure vib/Rpaccinterface.py
@@ -1,17 +1,50 @@
 import sys
-import redpitaya_scpi as scpi
-import matplotlib.pyplot as plt
 import time
-import os
-from PyQt5.QtWidgets import (QWidget, QToolTip,
-    QPushButton, QApplication)
-from PyQt5.QtGui import QFont
+from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 
-def Main()
+class WorkerThread(QThread):
+    # Определяем сигнал для передачи данных в основной поток
+    update_signal = pyqtSignal(str)
 
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        """Код, выполняемый в отдельном потоке"""
+        while 1:
+            print(1)
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Многопоточность в PyQt5')
+
+        self.start_button = QPushButton('start', self)
+        self.start_button.clicked.connect(self.start_thread)
+
+        self.stop_button = QPushButton('stop', self)
+        self.stop_button.move(0, 20)
+        self.stop_button.clicked.connect(self.stop_thread)
+
+        self.thread = WorkerThread()
+
+
+
+    def start_thread(self):
+        self.thread.start()  # Запускаем поток
+
+    def stop_thread(self):
+        self.thread.quit()
+        self.thread.wait()  
 
 if __name__ == '__main__':
-
     app = QApplication(sys.argv)
-    ex = Main()
+    window = MainWindow()
+    window.show()
     sys.exit(app.exec_())
