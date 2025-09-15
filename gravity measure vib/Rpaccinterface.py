86f83244fcdc23feab63ebdaf4c6fbb9f4c08129
KirillAdPC <ofadamovkirill@gmail.ru>
2025-09-15 12:39:42 +0700

    15.09.25 afternoom home

--- gravity measure vib/Rpaccinterface.py
+++ gravity measure vib/Rpaccinterface.py
@@ -1,47 +1,77 @@
 import sys
 import time
-from PyQt5.QtCore import QThread, pyqtSignal
+import io
+from PyQt5.QtCore import QRunnable, QThreadPool, pyqtSlot, QObject, pyqtSignal
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 
-class WorkerThread(QThread):
-    # Определяем сигнал для передачи данных в основной поток
-    update_signal = pyqtSignal(str)
 
+
+class WorkerSignals(QObject):
+    finished = pyqtSignal()
+    progress = pyqtSignal(int)
+
+
+class Worker(QRunnable):
     def __init__(self):
         super().__init__()
+        self.signals = WorkerSignals()
+        self._is_running = True
 
+    @pyqtSlot()
     def run(self):
-        """Код, выполняемый в отдельном потоке"""
-        while 1:
-            print(1)
+        i = 0
+        print("Поток запущен")
+        while self._is_running:
+            print(f"Работаю: {i}")
+            self.signals.progress.emit(i)
+            i += 1
+            time.sleep(0.1)  # чтобы не грузить CPU
+        print("Поток завершён")
+        self.signals.finished.emit()
+
+    def stop(self):
+        print("Остановка потока")
+        self._is_running = False
+
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
+        self.threadpool = QThreadPool()
+        self.worker = None
 
     def initUI(self):
-
         self.setGeometry(300, 300, 300, 200)
-        self.setWindowTitle('Многопоточность в PyQt5')
+        self.setWindowTitle('QRunnable + QThreadPool')
 
-        self.start_button = QPushButton('start', self)
-        self.start_button.clicked.connect(self.start_thread)
+        self.start_button = QPushButton('Start', self)
+        self.start_button.clicked.connect(self.start_worker)
 
-        self.stop_button = QPushButton('stop', self)
-        self.stop_button.move(0, 20)
-        self.stop_button.clicked.connect(self.stop_thread)
+        self.stop_button = QPushButton('Stop', self)
+        self.stop_button.move(0, 30)
+        self.stop_button.clicked.connect(self.stop_worker)
 
-        self.thread = WorkerThread()
+    def start_worker(self):
+        if self.worker is None:
+            self.worker = Worker()
+            self.worker.signals.finished.connect(self.worker_finished)
+            self.worker.signals.progress.connect(self.worker_progress)
+            self.threadpool.start(self.worker)
+        else:
+            print("Поток уже запущен")
 
+    def stop_worker(self):
+        if self.worker is not None:
+            self.worker.stop()
 
+    def worker_progress(self, val):
+        print(f"Прогресс: {val}")
 
-    def start_thread(self):
-        self.thread.start()  # Запускаем поток
+    def worker_finished(self):
+        print("Worker завершил выполнение")
+        self.worker = None
 
-    def stop_thread(self):
-        self.thread.quit()
-        self.thread.wait()  
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
