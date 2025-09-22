cc700a1445a8bea00d889c824b89e348c0b868bf
KirillAdILF <k.adamov@g.nsu.ru>
2025-09-22 18:49:17 +0700

    before binary read 2

--- gravity measure vib/RPaccRPsender.py
+++ gravity measure vib/RPaccRPsender.py
@@ -1,3 +1,7 @@
 # sender.py
-with open('start.txt', 'w', encoding='utf-8') as file:
-    file.write('1')
+def write_char(file_path, char):
+    with open(file_path, 'w', encoding='utf-8') as f:
+        f.write(char)
+
+path = 'C:/Users/MakarovAO/Desktop/Adamov_Kirill/gravity_measurements/gravity measure vib/testdata'
+write_char("%s/stat.txt" % path, '1')
